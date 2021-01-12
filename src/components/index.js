e037d09e91d85bab0b94a7c1d2da29bbac66ff90
Alexis <a.goutorbe@gmail.com>
2021-01-12 14:36:52 +0900

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// Place all your components here, for easy and clearer view
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
